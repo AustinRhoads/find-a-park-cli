7de551f414d8e75cf626a0c57340c74b9c6ca631
Austin Rhoads <austinrrhoads@ymail.com>
2020-04-06 17:36:10 +0000

    scrape state info

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,13 +4,21 @@ require 'nokogiri'
 
 class Scraper
   
-  def initialize(url)
-    
+  def self.scrape_states(url)
+    arr = []
     doc = Nokogiri::HTML(open(url))
     #states names - doc.css('form option').each {|x| puts x.text}
     #states name from map -  doc.css('map area/@alt').each   #{|x| puts x.text}
      #states url - doc.css('map area/@href').each {|x| puts url + x.text}
       #states url - doc.css('map area/@href').each {|x| puts url + x.text}
+      hash = {}
+
+    doc.css('map area').each do |state|
+      hash[:name] = state.css(/@alt).text
+      hash[:state_url] = state.css(/@href).text 
+      arr << hash
+    end
+      
     binding.pry
   end
   
