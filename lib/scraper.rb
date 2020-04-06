756df83f0afea29318a41f81996a5bd0c75a89be
Austin Rhoads <austinrrhoads@ymail.com>
2020-04-06 15:42:45 +0000

    found states url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,8 @@ class Scraper
     doc = Nokogiri::HTML(open(url))
     #states names - doc.css('form option').each {|x| puts x.text}
     #states name from map -  doc.css('map area/@alt').each   #{|x| puts x.text}
+     #states url - doc.css('map area/@href').each {|x| puts url + x.text}
+      #states url - doc.css('map area/@href').each {|x| puts url + x.text}
     binding.pry
   end
   
