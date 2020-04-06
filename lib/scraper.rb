97d5202cb0142e0b8ba89049ffe76c29886f49f3
Austin Rhoads <austinrrhoads@ymail.com>
2020-04-06 17:47:30 +0000

    state init and make_from_collection method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,10 +7,7 @@ class Scraper
   def self.scrape_states(url)
     arr = []
     doc = Nokogiri::HTML(open(url))
-    #states names - doc.css('form option').each {|x| puts x.text}
-    #states name from map -  doc.css('map area/@alt').each   #{|x| puts x.text}
-     #states url - doc.css('map area/@href').each {|x| puts url + x.text}
-      #states url - doc.css('map area/@href').each {|x| puts url + x.text}
+  
       
 
     doc.css('map area').each do |state|
--- lib/state.rb
+++ lib/state.rb
@@ -1,7 +1,16 @@
 class State
   
-  def initialize(hash)
-    
+  attr_accessor :name, :state_url 
+  
+  @@all = []
+  
+    def initialize(state_hash)
+state_hash.each { |k, v| self.send("#{k}=", v) }
+@@all << self
+  end
+  
+  def self.make_from_collection(states_array)
+    states_array.each {|state_hash| State.new(state_hash) }
   end
   
 end
