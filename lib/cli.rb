3b951fbfa47b128c50bf87201a5a171454f8386c
Austin Rhoads <austinrrhoads@ymail.com>
2020-04-07 21:59:46 +0000

    still starting over

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,30 +2,33 @@ require_relative "../lib/scraper.rb"
 require_relative "../lib/state.rb"
 require_relative "../lib/park.rb"
 
-# "https://www.nps.gov/findapark/advanced-search.htm"
+
 
 class CommandLineInterface
   
+  @@URL = "https://www.nps.gov/findapark/advanced-search.htm"
+  
  def self.run 
-   
-#   @@BASE_PATH = "https://www.nps.gov"
-   
-#url = @@BASE_PATH + '/findapark/index.htm'
-#states_array = Scraper.scrape_states(url)
-#State.make_from_collection(states_array)
-#states_names = []
-#State.all.each {|x| states_names << x.name}
-
-#urls = Scraper.park_urls(State.all.first.state_url)
-#Scraper.scrape_park(urls[1])
+   puts "hey"
+end
 
 
 
-url = "https://www.nps.gov/findapark/advanced-search.htm"
+def options 
 
-states_array = Scraper.scrape_states(url)
+
+states_array = Scraper.scrape_states(@@URL)
 
 end
+
+
+
+
+
+
+
+
+
 
 #end of class
 
