4680792ecb3ecdb4d6086baeb520ccb667314030
Austin Rhoads <austinrrhoads@ymail.com>
2020-04-15 00:04:01 +0000

    deleting old methods

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,19 +7,14 @@ class Scraper
   
   @@URL = "https://www.nps.gov/findapark/advanced-search.htm"
   
- # @@state_options = Scraper.scrape_state_names(@@URL)
-  
- 
- 
  
   def self.scrape_state_names(url)
     
     arr = []
    
-   doc = Nokogiri::HTML(open(url))
-  
- # binding.pry 
-  doc.css('select#form-park option').each do |state_name|
+    doc = Nokogiri::HTML(open(url))
+   
+    doc.css('select#form-park option').each do |state_name|
     
     next if state_name.text == "State"
     hash = {}
@@ -31,21 +26,10 @@ class Scraper
   return arr
   
   
-   #end of scrape_states
+   #end of scrape_states_names
     end
   
-  
-  def self.park_urls(url)
-   # arr = []
-    
-    #doc = Nokogiri::HTML(open(url))
-    
-    #doc.css('ul h3 a/@href').each do |href|
-    #url = @@BASE_PATH + href.text + "index.htm"
-    #arr << url
- # end
-    # return arr
-  end
+
   
   def self.scrape_state(num)
     arr = Scraper.states_array
@@ -78,19 +62,6 @@ class Scraper
   end
   
   
-  
-  def self.scrape_park(url)
-  #  arr = []
-   # doc = Nokogiri::HTML(open(url))
-    #park name - doc.css('a.Hero-title').text
-    #park mailing location -  doc.css("p.adr").text.tr("\n", "")
-    #park phone - doc.css('span.tel').text.tr("\n", "")
-    #basic info url - @@BASE_PATH + doc.css('.UtilityNav a/@href').first.text
-    #basic_info_url = @@BASE_PATH + doc.css('.UtilityNav a/@href').first.text
-  #  i_doc = Nokogiri::HTML(open(basic_info_url))
-   # binding.pry
-    
-  end
   
   def self.states_array 
     Scraper.scrape_state_names(@@URL)
@@ -144,5 +115,5 @@ end
 end
 
 
-#inlinescript = doc.xpath('//script[not(@src)]').text
+
 #new_doc = Nokogiri::HTML(open('https://www.nps.gov/findapark/find_a_park.json?dt=1586816373644'))
