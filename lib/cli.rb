6535c9a64f29f3095080b5aa8f244aa6ffafda42
Austin Rhoads <austinrrhoads@ymail.com>
2020-05-05 23:03:22 +0000

    display consistancy

--- lib/cli.rb
+++ lib/cli.rb
@@ -37,7 +37,7 @@ def state_options
     puts "#{index + 1}...#{hash[:name]}"
   end
   
-  puts "-----------------------"
+  puts "------------------------------------------------------"
   puts "Please enter the number of the state you'd wish to explore."
   
   #num = gets.chomp.to_i - 1
@@ -135,7 +135,7 @@ end
 def display_choice(choice) 
   puts "Park Name:            "+choice.name 
   puts "Park location:        "+ choice.location.gsub("PO Box ", "")
-  puts "States with access:   " + choice.state_code_list.to_s
+  puts "States with access:   " + choice.state_code_list.to_s #replace this with state obj list
   puts "Park url :            " + choice.url
   puts "Park Phone no.        "+choice.phone
   puts "------------------------------------------------------"
