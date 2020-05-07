6f3694f49bc4776167715b5c5b154b5101ab7e1b
Austin Rhoads <austinrrhoads@ymail.com>
2020-05-07 22:50:18 +0000

    no longer rescrapes states list

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,10 +9,11 @@ class CommandLineInterface
   @@URL = "https://www.nps.gov/findapark/advanced-search.htm" 
   @@ex_url = "https://www.nps.gov/findapark/find_a_park.json?dt=1586816373644"
   @@activity_options_3 = []
-  
+  @@state_options_3 = []
   
  def run 
-   #make_activity_list
+   make_activity_list(@@URL)
+   make_states_array
    state = state_options
    state_search(state)
    
@@ -33,7 +34,8 @@ end
 
 
 def state_options 
-  arr = Scraper.states_array
+ # arr = Scraper.states_array
+ arr = @@state_options_3
 
   arr.each_with_index do |hash, index|
     puts "#{index + 1}...#{hash[:name]}"
@@ -56,8 +58,8 @@ end
 
 def activity_options(state)
   
-  make_activity_list(@@URL)
-arr = @@activity_options_3
+ # make_activity_list(@@URL)
+  arr = @@activity_options_3
 # binding.pry
   arr.each_with_index do |act, index|
     puts "#{index + 1}...#{act[:activity]}"
@@ -168,6 +170,12 @@ end
 def make_activity_list(url)
   if @@activity_options_3 == [] 
     @@activity_options_3 = Scraper.scrape_activity_options(url)
+  end 
+end 
+
+def make_states_array 
+  if @@state_options_3 == []
+    @@state_options_3 = Scraper.states_array
   end 
 end
 
